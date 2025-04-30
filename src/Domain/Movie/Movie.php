4c1c3ef82d32ca4debbf7553c76486d0ab88e00b
Dariusz Dzięgiel <dariusz.dziegiel@gmail.com>
2025-04-30 14:56:38 +0200

    feat(recommendation): Create Movie domain entity

---
+++ src/Domain/Movie/Movie.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Movie;
+
+readonly class Movie
+{
+    public function __construct(
+        private string $title
+    ) {}
+
+    public function title(): string
+    {
+        return $this->title;
+    }
+
+    public function isMultiWordTitle(): bool
+    {
+        return str_word_count($this->title) > 1;
+    }
+
+    public function isTitleStartsWithWLetterAndHaveEvenLength(): bool
+    {
+        return str_starts_with($this->title, 'W') && mb_strlen($this->title) % 2 === 0;
+    }
+
+    public function __toString(): string
+    {
+        return $this->title;
+    }
+}
